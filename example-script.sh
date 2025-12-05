a2e73588d546678bcf54099389773abb4751c404
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2025-12-05 19:09:12 +0800

    Modify script to navigate into Python directory
    Change directory to the extracted Python folder before installation and clean up afterwards.

--- example-script.sh
+++ example-script.sh
@@ -6,6 +6,7 @@ python_file="Python-$python_version.tar.xz"
 
 wget https://www.python.org/ftp/python/3.14.1/$python_file
 tar xf $python_file
+cd $python_file
 
 sudo apt install -y make build-essential libssl-dev zlib1g-dev \
   libbz2-dev libreadline-dev libsqlite3-dev wget curl \
@@ -18,4 +19,5 @@ make -j $(($(nproc) / 2 + 1))
 
 sudo make altinstall
 
+cd ..
 rm -r ./Python-3.14.1
